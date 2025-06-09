222010d929470748cd724c9cbb94ce33eaa5231a
ghadir chhade <153683791+ghadirchhade@users.noreply.github.com>
2025-06-09 23:38:00 +0300

    update the watch demo button

--- components/hero.jsx
+++ components/hero.jsx
@@ -43,7 +43,7 @@ const HeroSection = () => {
                    Get Started
                 </Button>
               </Link>
-              <Link href="/demo.mp4">
+              <Link href="/finPlanDemo.mp4">
                 <Button size="lg" variant="outline" className="px-8">
                    Watch Demo
                 </Button>
